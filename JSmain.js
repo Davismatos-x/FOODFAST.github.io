e87a80bfbd87bad45316ee19d73d149dfbd85492
Davismatos-x <143741585+Davismatos-x@users.noreply.github.com>
2023-09-28 19:46:25 -0500

    WEB

---
+++ JSmain.js
@@ -0,0 +1,12 @@
+const salirads = document.querySelector(".cerrar-ads");
+const ads2 = document.querySelector(".ads2");
+const ads3 = document.querySelector(".ads3");
+
+
+salirads.addEventListener("click", () => {
+    ads2.classList.add("hiden");
+    ads3.classList.add("hiden");
+})
+
+
+    
